23587ff6cb50ce1a1e131ba2c49ee3d97685c3ea
Paulo Renan <able.paulorenan@gmail.com>
2022-05-07 15:47:52 -0300

    login page

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -1,4 +1,78 @@
 <template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" md="8" sm="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-row>
+                  <v-col cols="12" md="8">
+                    <v-card-text class="mt-12">
+                      <h1 class="text-center display-2 teal--text text--accent-3 mb-4">
+                        Login
+                      </h1>
+                      <h4 class="text-center mlt-4">
+                        Please enter your credentials to login
+                      </h4>
+                      <v-form>
+                        <v-text-field
+                          label="Email"
+                          v-model="email"
+                          :rules="emailRules"
+                          prepend-icon="email"
+                          @keyup.native="validate"
+                          name="email"
+                          type="text"
+                          required
+                          color="teal accent-3"
+                        />
+                        <v-text-field
+                          id="password"
+                          label="Password"
+                          name="Password"
+                          prepend-icon="lock"
+                          v-model="password"
+                          :rules="passwordRules"
+                          @keyup.native="validate"
+                          type="password"
+                          color="teal accent-3"
+                          required
+                        />
+                      </v-form>
+                    </v-card-text>
+                    <div class="text-center mt-3 mb-5">
+                      <v-btn rounded color="teal accent-3" @click="login" dark>
+                        Login
+                      </v-btn>
+                    </div>
+                  </v-col>
+                  <v-col cols="12" md="4" class="teal accent-3">
+                    <v-card-text class="white--text mt-12">
+                      <h1 class="text-center display-1">
+                        Olá, Visitante!
+                      </h1>
+                      <h5 class="text-center">
+                        Faça cadastro para ver nossos incríveis produtos!
+                      </h5>
+                    </v-card-text>
+                    <div class="text-center mt-3 mb-9">
+                      <v-btn rounded outlined="" dark>
+                          Cadastre-se
+                      </v-btn>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<!-- <template>
   <div class="login">
     <div v-if="loggingIn" class="container-loading">
       <img src="/loading.gif" alt="Loading Icon">
@@ -10,65 +84,22 @@
       <button type="submit">Login</button>
     </form>
   </div>
-</template>
-
-<style scoped lang="scss">
-  .login {
-    border: 1px solid black;
-    border-radius: 5px;
-    padding: 1.5rem;
-    width: 300px;
-    margin-left: auto;
-    margin-right: auto;
-    position: relative;
-    overflow: hidden;
-    .container-loading {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: rgba(0,0,0,.3);
-      img {
-        width: 2rem;
-        height: 2rem;
-      }
-    }
-    form {
-      display: flex;
-      flex-flow: column;
-      *:not(:last-child) {
-        margin-bottom: 1rem;
-      }
-      input {
-        padding: .5rem;
-      }
-      button {
-        padding: .5rem;
-        background-color: lightgray;
-        border: 1px solid gray;
-        border-radius: 3px;
-        cursor: pointer;
-        &:hover {
-          background-color: lightslategray;
-        }
-      }
-    }
-  }
-</style>
+</template> -->
 
 <script>
   import { mapState, mapActions } from 'vuex';
+  import '../styles/Login.css';
 
   export default {
     data() {
       return {
         email: '',
-        password: ''
+        password: '',
+        step: 1,
       }
+    },
+    props: {
+      source: String
     },
     computed: {
       ...mapState([
@@ -89,3 +120,7 @@
     }
   }
 </script>
+
+<style>
+@import '../styles/Login.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import store from './store'
 import router from './router'
+import './styles/Login.css'
 
 Vue.config.productionTip = false
 
@@ -10,5 +13,8 @@ new Vue({
   vuetify,
   store,
   router,
+  icons: {
+    iconfont: 'md' || 'fa',
+  },
   render: h => h(App)
 }).$mount('#app')
